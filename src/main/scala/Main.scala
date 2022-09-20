ab261f0849f41e2ae274b80e25a871600abdb397
Addison Mink <addison.mink@example.com>
2022-09-19 19:46:40 -0500

    add renderer rectangle methods
    - Add Renderer.rectIO
    - Add Renderer.rectOutlineIO
    - Add Renderer.roundedRectIO
    - Add Renderer.roundedRectOutlineIO

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,12 +3,37 @@ import canvasui.*
 import org.scalajs.dom.*
 
 @main def main: Unit =
-  clearDemo()
+  clearDemoIO()
+  rectDemoIO()
+  rectOutlineDemoIO()
+  roundedRectDemoIO()
+  roundedRectOutlineDemoIO()
 
-def clearDemo(): Unit =
-  val canvas = org.scalajs.dom.document
-    .getElementById("clear-demo")
-    .asInstanceOf[HTMLCanvasElement]
-
-  val renderer = Renderer(canvas)
+def clearDemoIO(): Unit =
+  val renderer = Renderer(canvasIO("clear-demo"))
   renderer.clearIO(Color.black)
+
+def rectDemoIO(): Unit =
+  val renderer = Renderer(canvasIO("rect-demo"))
+  renderer.clearIO(Color.black)
+  renderer.rectIO(25, 40, 75, 100, Color.white)
+
+def rectOutlineDemoIO(): Unit =
+  val renderer = Renderer(canvasIO("rect-outline-demo"))
+  renderer.clearIO(Color.black)
+  renderer.rectOutlineIO(25, 40, 75, 100, 5, Color.white)
+
+def roundedRectDemoIO(): Unit =
+  val renderer = Renderer(canvasIO("rounded-rect-demo"))
+  renderer.clearIO(Color.black)
+  renderer.roundedRectIO(25, 40, 75, 100, 5, Color.white)
+
+def roundedRectOutlineDemoIO(): Unit =
+  val renderer = Renderer(canvasIO("rounded-rect-outline-demo"))
+  renderer.clearIO(Color.black)
+  renderer.roundedRectOutlineIO(25, 40, 75, 100, 5, 5, Color.white)
+
+def canvasIO(id: String): HTMLCanvasElement =
+  document
+    .getElementById(id)
+    .asInstanceOf[HTMLCanvasElement]
--- src/main/scala/canvasui/Renderer.scala
+++ src/main/scala/canvasui/Renderer.scala
@@ -10,3 +10,63 @@ class Renderer(canvas: HTMLCanvasElement):
   def clearIO(color: Color): Unit =
     ctx.fillStyle = color.cssCode
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  def rectIO(x: Int, y: Int, width: Int, height: Int, color: Color): Unit =
+    ctx.fillStyle = color.cssCode
+    ctx.fillRect(x, y, width, height)
+
+  def rectOutlineIO(
+      x: Int,
+      y: Int,
+      width: Int,
+      height: Int,
+      thickness: Int,
+      color: Color
+  ): Unit =
+    ctx.strokeStyle = color.cssCode
+    ctx.lineWidth = thickness
+    ctx.strokeRect(x, y, width, height)
+
+  def roundedRectIO(
+      x: Int,
+      y: Int,
+      width: Int,
+      height: Int,
+      radius: Int,
+      color: Color
+  ): Unit =
+    ctx.fillStyle = color.cssCode
+    roundedRectPathIO(x, y, width, height, radius)
+    ctx.fill()
+
+  def roundedRectOutlineIO(
+      x: Int,
+      y: Int,
+      width: Int,
+      height: Int,
+      thickness: Int,
+      radius: Int,
+      color: Color
+  ): Unit =
+    ctx.strokeStyle = color.cssCode
+    ctx.lineWidth = thickness
+    roundedRectPathIO(x, y, width, height, radius)
+    ctx.stroke()
+
+  private def roundedRectPathIO(
+      x: Int,
+      y: Int,
+      width: Int,
+      height: Int,
+      radius: Int
+  ): Unit =
+    ctx.beginPath()
+    ctx.moveTo(x + radius, y)
+    ctx.lineTo(x + width - radius, y)
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius)
+    ctx.lineTo(x + width, y + height - radius)
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height)
+    ctx.lineTo(x + radius, y + height)
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius)
+    ctx.lineTo(x, y + radius)
+    ctx.quadraticCurveTo(x, y, x + radius, y)
